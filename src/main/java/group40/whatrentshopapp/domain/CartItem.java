62025737edeb2ff5098e1ae1664c099851bb5720
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-07-07 22:36:38 +0200

    new entities added

--- src/main/java/group40/whatrentshopapp/domain/CartItem.java
+++ src/main/java/group40/whatrentshopapp/domain/CartItem.java
@@ -1,7 +1,6 @@
 package group40.whatrentshopapp.domain;
 
 import jakarta.persistence.*;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
