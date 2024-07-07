338e88e4b78a79dba505c4327829d22e02a69653
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-07-07 12:26:38 +0200

    Initial commit

---
+++ src/main/java/group40/whatrentshopapp/WhatRentShopAppApplication.java
@@ -0,0 +1,13 @@
+package group40.whatrentshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatRentShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatRentShopAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group40/whatrentshopapp/WhatRentShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group40.whatrentshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatRentShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
